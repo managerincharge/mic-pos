83a44cf944a547aaa0264632e11b28d467742b07
managerincharge <jim@managerincharge.com>
2013-11-05 05:02:51 -0800

    version 0.2.0

--- routes/enc-dec.js
+++ routes/enc-dec.js
@@ -8,12 +8,12 @@
 
  		enc: function(req, res){
  			var s = req.params.s;
- 			res.send(helper.vernam.enc(s, helper.settings.storeId));
+ 			res.send(helper.vernam.enc(s, helper.settings.storeId) + '<br>' + helper.settings.storeId + '<br>' + s);
  		},
 
  		dec: function(req, res){
  			var s = req.params.s;
- 			res.send(helper.vernam.dec(s, helper.settings.storeId));
+ 			res.send(helper.vernam.dec(s, helper.settings.storeId) + '<br>' + helper.settings.storeId + '<br>' + s);
  		}
 
  	};
--- settings/settings.development.js
+++ settings/settings.development.js
@@ -3,7 +3,7 @@ var settings = module.exports = {};
 settings.storeId = 'fcf-9999';
 
 settings.fbRootRef = 'https://mic-pos.firebaseio.com/';
-settings.firebaseKey = '°Ë¢¤ÊÈ¹w³³§¤®ÌÑc{²Ç²ªÚÊÙ|¨®';
+settings.firebaseKey = 'Éß¦¦¤¬²¶yn¸Ï¬c­½Ù¢w¤i«¹¿§i£';
 settings.firebaseSecurityDebug = true;
 
 settings.localLogFile = 'log.txt';
